53c59667d298a87e8382da919b6b0054fc6e923f
soma <mrsumomo7@gmail.com>
2024-05-11 13:35:30 +0900

    Initial commit

---
+++ src/main/java/jp/houlab/mochidsuki/tad_character_wizard/TAD_Character_Wizard.java
@@ -0,0 +1,17 @@
+package jp.houlab.mochidsuki.tad_character_wizard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TAD_Character_Wizard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
